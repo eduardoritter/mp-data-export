db37df7771fa011999278ad16bcc1fec04c7ce2b
Eduardo Ritter <eduardoritter@gmail.com>
2021-04-06 23:12:06 -0300

    update

--- src/controller/patient.js
+++ src/controller/patient.js
@@ -2,11 +2,21 @@ const uuid = require('uuid');
 
 module.exports = {
     show(req, res, next) {
-        try {
+        try { 
             console.log('Id:', req.params.id);            
-            res.send(`Get patient Id: ${req.params.id} - Request Id: ${uuid.v4()}`);        
+            res.send(`Get patient Id: ${req.params.id} - Request Id: ${uuid.v4()}`);              
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    plan(req, res, next) {
+        try { 
+            console.log('Id:', req.params.id);            
+            res.send(`Get active plan of patient Id: ${req.params.id} - Request Id: ${uuid.v4()}`);              
         } catch (e) {
             next(e);
         }
     }
+
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const patientController = require('../controller/patient');
 
 router.get('/patient/:id', patientController.show);
+router.get('/patient/:id/plan', patientController.plan);
 
 module.exports = router;
